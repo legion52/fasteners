3b30f8a6fa70d712c495797303b2e63eb3c1fd71
Ahmad <ahmad@hojikov.ru>
2024-09-22 00:18:32 +0300

    adaptive version of header

--- fasteners-app/src/app/header/index.tsx
+++ fasteners-app/src/app/header/index.tsx
@@ -1,28 +1,32 @@
 'use client'
 import React, { useState } from 'react'
 import Logo from '../shared/assets/icon/logo.svg';
+import Menu from '../shared/assets/icon/menu.svg';
 import styles from './header.module.css'
 const Header = () => {
     const [primary, setPrimary] = useState(true);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const tabs = ['О КОМПАНИИ', 'ПРОИЗВОДСТВО', 'КАТАЛОГ', 'ПАРТНЕРЫ', 'НОВОСТИ', 'КОНТАКТЫ',]
     return (
         <div
-            className={styles.container}
+            className={` ${isMenuOpen ? styles.containerOpen : styles.container}`}
             style={{
                 backgroundImage: `url(/images/${primary ? 'header.png' : 'header1.png'})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                height: '700px'
+                backgroundSize: 'cover'
             }}
 
         >
-            <div className={styles.navbar}><Logo onClick={() => { setPrimary(prev => !prev) }} className={styles.logo} />
+            <div className={styles.navbar}>
+                <div className={styles.logoBlock}>
+                    <Logo onClick={() => { setPrimary(prev => !prev) }} className={styles.logo} />
+                    <span>+7 (999) 999 99-99</span>
+                    <Menu onClick={() => {
+                        setIsMenuOpen(prev => !prev);
+                    }}
+                        className={styles.logoMenu} />
+                </div>
                 <div className={styles.menu}>
-                    <span >О КОМПАНИИ</span>
-                    <span >ПРОИЗВОДСТВО</span>
-                    <span >КАТАЛОГ</span>
-                    <span >ПАРТНЕРЫ</span>
-                    <span >НОВОСТИ</span>
-                    <span >КОНТАКТЫ</span>
+                    {tabs.map(el => (<span key={el}>{el}</span>))}
                 </div>
             </div></div>
     )
